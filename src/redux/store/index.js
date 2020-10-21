493be38299976f66b04cff5708c29aa80742b1b4
kgg05 <kgg05@mail.ru>
2020-10-21 13:37:05 +0700

    delete redux_devtools in store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -17,7 +17,7 @@ const reducer = combineReducers({
 });
 
 const store = createStore(reducer,
-    compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
     );
 
 export default store;
